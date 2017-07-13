84e85441e6b7f1a40e092917399e4b86c7b70439
wanglu <wang_l@youzu.com>
2017-07-13 17:14:55 +0800

    ping接口功能完善及代码优化

--- bbssdk/table/table_bbssdk_forum_sync.php
+++ bbssdk/table/table_bbssdk_forum_sync.php
@@ -2,8 +2,8 @@
 if(!defined('IN_DISCUZ')) {
 	exit('Access Denied');
 }
-
-class table_bbssdk_forum_sync extends discuz_table
+require_once 'bbssdk_common_sync.php';
+class table_bbssdk_forum_sync extends bbssdk_common_sync
 {
 	public function __construct()
 	{
@@ -11,26 +11,5 @@ class table_bbssdk_forum_sync extends discuz_table
 		$this->_pk = "syncid";
 
 		parent::__construct();
-	}
-
-	public function count_by_unsync()
-	{
-		return (int) DB::result_first("SELECT COUNT(*) FROM %t WHERE synctime = 0", array($this->_table));
-	}	
-	
-	public function unsync_list($start = 0, $limit = 0, $sort = 'desc')
-	{
-		if($sort){
-			$this->checkpk();
-		}
-		return DB::fetch_all("select * from ".DB::table($this->_table) . " where synctime = 0 order by ".DB::order($this->_pk,$sort) . DB::limit($start, $limit));
-	}
-
-	public function change_status($ids)
-	{
-		$idstirng = is_array($ids) ? join(',',$ids) : $ids;
-		if(!preg_match("%[\d\,]+%is", $idstirng))
-			return false;
-		return DB::query('update '.DB::table($this->_table).' SET synctime=unix_timestamp(now()) where syncid in('.$idstirng.')');
 	}
 }
