7df71af445b027c9093c33e174ec00b3d5aec41d
wanglu <wang_l@youzu.com>
2017-07-13 17:14:55 +0800

    ping接口功能完善及代码优化

--- bbssdk/controller/ping_ctrl.php
+++ bbssdk/controller/ping_ctrl.php
@@ -1,6 +1,11 @@
 <?php
 if(!defined('DISABLEDEFENSE'))  exit('Access Denied!');
 require_once 'table/table_bbssdk_favorite_sync.php';
+require_once 'table/table_bbssdk_menu_sync.php';
+require_once 'table/table_bbssdk_member_sync.php';
+require_once 'table/table_bbssdk_forum_sync.php';
+require_once 'table/table_bbssdk_comment_sync.php';
+
 class Ping extends BaseCore
 {
 	function __construct()
@@ -10,6 +15,7 @@ class Ping extends BaseCore
 	public function get_new()
 	{
             $t = intval($_GET['t']);
+            //收藏
             $favorites = $syncids = [];
             $favs = c::t('bbssdk_favorite_sync')->unsync_list_by_time($t,100);
             if($favs){
@@ -21,9 +27,75 @@ class Ping extends BaseCore
                 }
                 c::t('bbssdk_favorite_sync')->change_status($syncids);
             }
+            //论坛版块
+            $fids = $syncids = [];
+            $menus = c::t('bbssdk_menu_sync')->unsync_list_by_time($t,3);
+            if($menus){
+                foreach ($menus as $menu){
+                    array_push($syncids, $menu['syncid']);
+                    
+                    $menu['fid']*=$menu['flag']==3?-1:1;
+                    array_push($fids, $menu['fid']);
+                }
+                c::t('bbssdk_menu_sync')->change_status($syncids);
+            }
+            //用户组
+            $groupids = $syncids = [];
+            $usergroups = c::t('bbssdk_usergroup_sync')->unsync_list_by_time($t,3);
+            if($usergroups){
+                foreach ($usergroups as $usergroup){
+                    array_push($syncids, $usergroup['syncid']);
+                    
+                    $usergroup['groupid']*=$usergroup['flag']==3?-1:1;
+                    array_push($groupids, $usergroup['groupid']);
+                }
+                c::t('bbssdk_usergroup_sync')->change_status($syncids);
+            }
+            $uids = $syncids = [];
+            $members = c::t('bbssdk_member_sync')->unsync_list_by_time($t,3);
+            if($members){
+                foreach ($members as $member){
+                    array_push($syncids, $member['syncid']);
+                    
+                    $member['uid']*=$member['flag']==3?-1:1;
+                    array_push($uids, $member['uid']);
+                }
+                c::t('bbssdk_member_sync')->change_status($syncids);
+            }
+            //主题
+            $threads = $syncids = [];
+            $forums = c::t('bbssdk_forum_sync')->unsync_list_by_time($t,3);
+            if($forums){
+                foreach ($forums as $forum){
+                    array_push($syncids, $forum['syncid']);
+                    
+                    $forum['fid']*=$forum['flag']==3?-1:1;                    
+                    array_push($threads, $forum['fid'].'#'.$forum['tid']);
+                }
+                c::t('bbssdk_forum_sync')->change_status($syncids);
+            }
+            //评论
+            $posts = $syncids = [];
+            $comments = c::t('bbssdk_comment_sync')->unsync_list_by_time($t,3);
+            if($comments){
+                foreach ($comments as $comment){
+                    array_push($syncids, $comment['syncid']);
+                    
+                    $comment['fid']*=$comment['flag']==3?-1:1;                    
+                    array_push($posts, $comment['fid'].'#'.$comment['tid'].'#'.$comment['pid']);
+                }
+                c::t('bbssdk_comment_sync')->change_status($syncids);
+            }
             
             $data['time'] = DB::fetch_first('select UNIX_TIMESTAMP(NOW()) as timestamp')['timestamp'];
-            $data['favorites'] = $favorites;
+            
+            $data['favorites']  = $favorites;
+            $data['forums']     = $fids;
+            $data['usergroups'] = $groupids;
+            $data['users']      = $uids;
+            $data['threads']    = $threads;
+            $data['posts']      = $posts;
+            
             $this->success_result($data);
 	}
 }
